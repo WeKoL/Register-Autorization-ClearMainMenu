856fe58a6f4f415be4fe101457a17b3f0e51a5ad
WeKoL <114169561+WeKoL@users.noreply.github.com>
2024-04-30 21:01:38 +0400

    Add files via upload

---
+++ program/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR_Michalev.program
+{
+    internal class Userinfo
+    {
+        public string login { get; set; }
+        public string pass { get; set; }
+        public bool isAdmin { get; set; }
+        public bool isNewUser { get; set; }
+        public bool isWorker { get; set; }
+    }
+}
